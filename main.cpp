1410e3ea31eb39827855c40f53aaeb1138962bd4
Natan Siller Laurett <natan_siller@hotmail.com>
2020-10-06 17:09:54 -0300

    Incorporação das práticas

--- main.cpp
+++ main.cpp
@@ -6,147 +6,125 @@
 #include "CarregarArquivo.cpp"
 using namespace std;
 
-static int handx = 0, fingerfinalz = 0, shoulderx = 0, shouldery = 0,
-                            shoulderz = 0, elbow = 0;
+#define PI 3.1415
 
+static int Rot_carro = 0;
+static float Trans_carro_x = 0.0, Trans_carro_z = 0.0, Velocidade = 0.0;
 GLfloat angle = 60, fAspect;
-CarregarArquivo modelo;
 
 void Inicializa(void){
   glClearColor (1.0, 1.0, 1.0, 0.0);
-  glColor3f(0.0, 0.3, 0.2);
+  glColor3f(0.4, 0.30, 0.68);
   angle = 60;
-  modelo.Carregar("E:/UFOP/TrabalhoFinalCG/TruckNoTire.obj");
+  glEnable(GL_DEPTH_TEST);
 }
 
-void myModel(float scale){
-    glPushMatrix();
-    glScalef(scale,scale,scale);
-    for (unsigned int j = 0; j < (modelo.faces).size(); ++j )
-    {
+void Piso(float scale, float height){
+  glPushMatrix();
+  glTranslatef(0.0, height, 0.0);
+  glScalef(scale,scale,scale);
+  glColor3f(0.1, 0.15, 0.28);
+  glBegin(GL_POLYGON);
+  glVertex3f(100.0, 0.0, 100.0);
+  glVertex3f(-100.0, 0.0, 100.0);
+  glVertex3f(-100.0, 0.0, -100.0);
+  glVertex3f(100.0, 0.0, -100.0);
+  glEnd();
+  glPopMatrix();
+}
 
-        glBegin ( GL_POLYGON );
-
-        for (unsigned int i = 0; i < (modelo.faces[j]).size() ; ++i )        {
-            GLfloat vert[3] = {(modelo.vertices[modelo.faces[j][i][0]][0]),(modelo.vertices[modelo.faces[j][i][0]][1]),(modelo.vertices[modelo.faces[j][i][0]][2])};
-            glVertex3fv ( vert );
-        }
-
-        glEnd( );
+void myModel(float scale, CarregarArquivo modelo){
+  glPushMatrix();
+  glScalef(scale,scale,scale);
+  for (unsigned int j = 0; j < (modelo.faces).size(); ++j){
+    glBegin ( GL_POLYGON );
+    for (unsigned int i = 0; i < (modelo.faces[j]).size() ; ++i ){
+      GLfloat vert[3] = {(modelo.vertices[modelo.faces[j][i][0]][0]),(modelo.vertices[modelo.faces[j][i][0]][1]),(modelo.vertices[modelo.faces[j][i][0]][2])};
+      glVertex3fv ( vert );
     }
-    glPopMatrix();
+    glEnd();
+  }
+  glPopMatrix();
+}
 
-
+void Carro(int rot_y,float trans_x,float trans_z){
+  CarregarArquivo modelo;
+  modelo.Carregar("E:/UFOP/TrabalhoFinalCG/TruckNoTire.obj");
+  glPushMatrix();
+  glTranslatef(trans_x,0.0,trans_z);
+  glRotatef(rot_y + 90,0.0,1.0,0.0);
+  glScalef(1.0,1.0,2.0);
+  glColor3f(0.45,0.6,0.4);
+  myModel(0.005, modelo);
+  glPopMatrix()
 }
 
 void Desenha(void){
-  glClear (GL_COLOR_BUFFER_BIT);
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glPushMatrix();
-    glRotatef(90,0,1,0);
-    myModel(0.005);
-  glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(Trans_carro_x - 20 * sin(Rot_carro*PI/180),4,Trans_carro_z - 20 * cos(Rot_carro*PI/180), Trans_carro_x,0,Trans_carro_z, 0,1,0); // Especifica posição do observador e do alvo
 
-  glutSwapBuffers();
+    glPushMatrix();
+    Piso(1.0, -1.0);
+    Carro(Rot_carro, Trans_carro_x, Trans_carro_z);
+
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
-
-
-
-void AlteraTamanhoJanela (int w, int h){
-  glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-  if(h == 0) h = 1;
-
-  glMatrixMode (GL_PROJECTION);
-  glLoadIdentity ();
-  gluPerspective(65, (GLfloat) w/(GLfloat) h, 0.5, 500);
-
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  gluLookAt(0,0,13, 0,0,0, 0,1,0); // Especifica posição do observador e do alvo
-}
 
 void Teclado(unsigned char key, int x, int y){
   switch (key) {
-  case 's':
-    shoulderx= (shoulderx + 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'S':
-    shoulderx= (shoulderx - 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'h':
-    handx = (handx + 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'H':
-    handx= (handx - 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'f':
-    fingerfinalz = (fingerfinalz + 5) % 360;
-    if(fingerfinalz >= 0)fingerfinalz = 0;
-    glutPostRedisplay();
-    break;
-  case 'F':
-    fingerfinalz = (fingerfinalz - 5) % 360;
-    if(fingerfinalz <= -90)fingerfinalz = -90;
-    glutPostRedisplay();
-    break;
-  case 'a':
-    shouldery= (shouldery + 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'A':
-    shouldery= (shouldery - 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'd':
-    shoulderz = (shoulderz+ 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'D':
-    shoulderz = (shoulderz- 5) % 360;
-    glutPostRedisplay();
-    break;
-  case 'e':
-    elbow = (elbow + 5) % 360;
-    if(elbow > 120 % 360) elbow = 120;
-    glutPostRedisplay();
-    break;
-  case 'E':
-    elbow = (elbow - 5) % 360;
-    if(elbow < -120) elbow = -120;
-    glutPostRedisplay();
-    break;
-  case 27:
-    exit(0);
-    break;
-  default:
-    break;
+    case 's':
+      if (Velocidade > 0.02)
+        Velocidade -= 0.03;
+      else if (Velocidade > 0)
+        Velocidade -= -0.01;
+      else
+        Velocidade = 0.0;
+      glutPostRedisplay();
+      break;
+
+    case 'w':
+      if (Velocidade < 0.1)
+        Velocidade += 0.01;
+      glutPostRedisplay();
+      break;
+
+    case 'a':
+      Rot_carro = (Rot_carro + 10) % 360;
+      glutPostRedisplay();
+      break;
+
+    case 'd':
+      Rot_carro = (Rot_carro - 10) % 360;
+      glutPostRedisplay();
+      break;
   }
+}
+
+void idle(){
+  Trans_carro_x = Trans_carro_x + Velocidade*sin(Rot_carro*PI/180);
+  Trans_carro_z = Trans_carro_z + Velocidade*cos(Rot_carro*PI/180);
+
+  glutPostRedisplay();
 }
 
 int main(int argc, char** argv){
   glutInit(&argc, argv);
-
   glutInitDisplayMode (GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
-
   glutInitWindowSize (800, 800);
-
   glutInitWindowPosition ((1280 - 800)/2, (800 - 800)/2);
 
-  glutCreateWindow ("Computação Gráfica: Braço Mecânico Aramado");
+  glutCreateWindow ("Computação Gráfica: Movimento e Perspectiva");
 
   Inicializa();
-
   glutDisplayFunc(Desenha);
-
   glutReshapeFunc(AlteraTamanhoJanela);
-
   glutKeyboardFunc(Teclado);
-
+  glutIdleFunc(idle);
   glutMainLoop();
   return 0;
 }
